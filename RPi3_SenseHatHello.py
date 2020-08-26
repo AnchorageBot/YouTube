6ad869d0828d4911b3382b676f4d91a6de6cbcf1
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2020-08-25 16:24:33 -0800

    Update and rename RPi3SenseHatHello to RPi3_SenseHatHello.py

--- RPi3_SenseHatHello.py
+++ RPi3_SenseHatHello.py
@@ -5,7 +5,10 @@
 #    RPi 3B+ https://www.raspberrypi.org/products/raspberry-pi-3-model-b-plus/
 #    SenseHAT https://www.raspberrypi.org/products/sense-hat/
 #
-#
+# ATS
+# https://github.com/AnchorageBot
+
+
 #  Libraries
 from sense_hat import SenseHat
 #
