2b9e4b4f9abf98be243792452a1658b8810f8d8a
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2020-12-23 08:07:25 -0900

    Update cPyThermBlink.py

--- cPyThermBlink.py
+++ cPyThermBlink.py
@@ -1,5 +1,24 @@
 # Read/Print Thermistor Values via Serial and Blink LED using a CircuitPlayground Bluefruit and Circuit Python
 
+# Dr Gallaugher's CPB Tutorials
+    # https://www.youtube.com/playlist?list=PL9VJ9OpT-IPSsQUWqQcNrVJqy4LhBjPX2
+
+# Adafruit CPB temperature tutorial
+  # https://learn.adafruit.com/adafruit-circuit-playground-bluefruit/playground-temperature
+  
+# Adafruit CircuitPython libraries
+    # https://learn.adafruit.com/welcome-to-circuitpython/circuitpython-libraries
+    # https://circuitpython.org/libraries
+    # https://circuitpython.readthedocs.io/projects/thermistor/en/latest/
+    # https://circuitpython.readthedocs.io/projects/thermistor/en/latest/api.html#implementation-notes
+   
+# Materials
+  # Circuit Playground Bluefruit https://www.adafruit.com/product/4333
+  # USB A to Micro B data cable https://www.adafruit.com/product/592
+
+# Software
+    # Mu Python Editor https://codewith.mu
+
 import time
 import board
 import adafruit_thermistor
