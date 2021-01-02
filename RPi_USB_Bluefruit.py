520d0004bbfae39a9caccf8d98d03e0747c9b43a
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2021-01-02 11:19:11 -0900

    Create RPi_USB_Bluefruit.py
    Initial commit

---
+++ RPi_USB_Bluefruit.py
@@ -0,0 +1,22 @@
+# This code will provide serial communication between a RPi and a Bluefruit Sense without the Arduino IDE
+# This code is loaded on the RPi while Bluefruit_USB_RPi.ino is loaded on the Bluefruit
+
+# Software and materials list
+    # Programming the RPi, 2nd Ed, by Simon Monk http://simonmonk.org/programming-raspberry-pi-ed2/
+    # RPi 3B+ or RPi Zero https://www.adafruit.com/product/3400 
+    # USB A to Micro B Cable https://www.adafruit.com/product/592 
+    # Adafruit Bluefruit Sense https://www.adafruit.com/product/4516
+
+# Connect RPi and Bluefruit via USB cable
+# Code is run in the Python 3 Shell on the RPi
+
+
+# Start RPi in listening mode (Bluefruit is in talking mode)
+import serial
+ser = serial.Serial('/dev/ttyACM0', 9600)
+while True:
+    print(ser.readline())
+    
+# Interupt Bluefruit using ctrl-c on the RPi keyboard then load & run code to blink Bluefruit LED 5X
+# Puts RPi in talking mode (Bluefruit is in listening mode)
+ser.write('5'.encode())
