c4b8f43106045a2a53c042126042d7fe21a69684
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2021-01-24 17:27:41 -0900

    Create cPyForVarList.py

---
+++ cPyForVarList.py
@@ -0,0 +1,39 @@
+# This script uses a for loop, Variables & Lists to light neopixels on the CPB with CircuitPython
+
+# Coding concepts used in the script
+
+    # A for loop will repeat indented commands a specified number of times
+        # Dr Gallaugher's CPB Tutorials
+        # https://www.youtube.com/watch?v=v-mGgjM6leo&list=PL9VJ9OpT-IPSsQUWqQcNrVJqy4LhBjPX2&index=7
+        # https://www.youtube.com/playlist?list=PL9VJ9OpT-IPSsQUWqQcNrVJqy4LhBjPX2
+
+    # Variables are symbolic names that reference or point (pointer) to an object.
+        # Once an object is assigned to a variable, you can refer to the object by that name.
+    # https://realpython.com/python-variables/
+
+    # A list is a data structure in Python that is a mutable (changeable) ordered sequence of elements.
+        # Each element or value that is inside of a list is called an item.
+        # Lists are defined by having values between square brackets [ ].
+    # https://www.digitalocean.com/community/tutorials/understanding-lists-in-python-3
+
+# Materials
+  # Circuit Playground Bluefruit (CPB) https://www.adafruit.com/product/4333
+  # USB A to Micro B data cable https://www.adafruit.com/product/592
+
+# Anchorage Tech Solutions YouTubes & Code
+    # https://www.youtube.com/channel/UCDuWq2wFqeVII1KC7grySRg?view_as=subscriber
+    # https://github.com/AnchorageBot/YouTube
+
+import board                # CircuitPython Library https://circuitpython.org/libraries
+import neopixel
+import time
+
+pixels = neopixel.NeoPixel(board.NEOPIXEL, 10, brightness = 0.25, auto_write=True)
+                            # https://learn.adafruit.com/circuitpython-essentials/circuitpython-neopixel
+                            # https://github.com/adafruit/Adafruit_CircuitPython_NeoPixel
+                            # pixels is a variable that also functions as a list
+
+for NeoPixelNumber in range(5):                 # Variable - NeoPixelNumber, range runs across the list [0, 1, 2,3,4]
+    pixels[NeoPixelNumber] = (255,0,255)
+    time.sleep(0.5)
+                                                # Program stops after one run
