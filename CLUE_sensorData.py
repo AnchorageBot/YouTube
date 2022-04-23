09df24d8b797d6d66c22c38b97d63f05bdf9c8f0
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2022-04-23 11:59:07 -0800

    Update CLUE_sensorData.py

--- CLUE_sensorData.py
+++ CLUE_sensorData.py
@@ -1,7 +1,9 @@
 # This script displays sensor and input data on the CLUE display and prints sensor data via serial
 # Script source code 
     # https://docs.circuitpython.org/projects/clue/en/latest/
+    # https://docs.circuitpython.org/projects/clue/en/latest/api.html
     # https://github.com/adafruit/Adafruit_CircuitPython_CLUE
+    # CLUE CircuitPython Tutorial: https://learn.adafruit.com/adafruit-clue/circuitpython    
 
 # Modified by ATS, April, 2022, https://github.com/AnchorageBot
 
@@ -22,9 +24,6 @@
     # adafruit_slideshow.mpy
     # neopixel.mpy
 
-# CLUE CircuitPython Tutorial: https://learn.adafruit.com/adafruit-clue/circuitpython
-# CLUE High Level Documentation https://docs.circuitpython.org/projects/clue/en/latest/api.html
-
 from adafruit_clue import clue
 
 clue.sea_level_pressure = 1020
@@ -33,14 +32,14 @@ clue.sea_level_pressure = 1020
 clue_data = clue.simple_text_display(text_scale = 2)
 
 while True:
-    clue_data[0].text = "Acceleration: {:.2f} {:.2f} {:.2f}".format(*clue.acceleration)
+    clue_data[0].text = "Accel: {:.2f} {:.2f} {:.2f}".format(*clue.acceleration)
     clue_data[1].text = "Gyro: {:.2f} {:.2f} {:.2f}".format(*clue.gyro)
-    clue_data[2].text = "Magnetic: {:.3f} {:.3f} {:.3f}".format(*clue.magnetic)
+    clue_data[2].text = "Mag: {:.3f} {:.3f} {:.3f}".format(*clue.magnetic)
     clue_data[3].text = "Pressure: {:.3f}hPa".format(clue.pressure)
     clue_data[4].text = "Altitude: {:.1f}m".format(clue.altitude)
     clue_data[5].text = "Temperature: {:.1f}C".format(clue.temperature)
     clue_data[6].text = "Humidity: {:.1f}%".format(clue.humidity)
-    clue_data[7].text = "Color: R: {} G: {} B: {} C: {}".format(*clue.color)    
+    clue_data[7].text = "R: {} G: {} B: {} C: {}".format(*clue.color)    
     #clue_data[8].text = "Proximity: {}".format(clue.proximity)
     #clue_data[9].text = "Gesture: {}".format(clue.gesture)
     #clue_data[10].text = "Button A: {}".format(clue.button_a)
@@ -50,14 +49,14 @@ while True:
     #clue_data[14].text = "Touch 2: {}".format(clue.touch_2)
     clue_data.show()
     
-    print("Acceleration: {:.2f} {:.2f} {:.2f} m/s^2".format(*clue.acceleration))
+    print("Accel: {:.2f} {:.2f} {:.2f} m/s^2".format(*clue.acceleration))
     print("Gyro: {:.2f} {:.2f} {:.2f} dps".format(*clue.gyro))
-    print("Magnetic: {:.3f} {:.3f} {:.3f} uTesla".format(*clue.magnetic))
+    print("Mag: {:.3f} {:.3f} {:.3f} uTesla".format(*clue.magnetic))
     print("Pressure: {:.3f} hPa".format(clue.pressure))
     print("Altitude: {:.1f} m".format(clue.altitude))
     print("Temperature: {:.1f} C".format(clue.temperature))
     print("Humidity: {:.1f} %".format(clue.humidity))
     print("Proximity: {}".format(clue.proximity))
     print("Gesture: {}".format(clue.gesture))
-    print("Color: R: {} G: {} B: {} C: {}".format(*clue.color))
+    print("R: {} G: {} B: {} C: {}".format(*clue.color))
     print("--------------------------------")
