c514294df31b340bc2004594ee1edbecf5617551
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2022-04-24 10:16:04 -0800

    Create RPi_getMCUdata.py

---
+++ RPi_getMCUdata.py
@@ -0,0 +1,30 @@
+# This script connects a RPi to a MCU (Microcontroller) and saves sensor data to a csv file
+    # csv file will be located in the same folder that this script resides in
+  
+# RPi Zero hardware kit
+  # https://www.adafruit.com/product/3410
+  
+# Source code references
+  # https://docs.python.org/3/index.html
+    # https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
+    # https://docs.python.org/3/library/datetime.html
+  # https://pyserial.readthedocs.io/en/latest/
+    # https://pyserial.readthedocs.io/en/latest/shortintro.html#opening-serial-ports
+  
+# Modified by ATS, April 2022
+
+import serial
+import datetime
+
+CSVfileName = "AK_CLUE_1.csv"
+
+openCommoLine = serial.Serial('/dev/ttyACM0', 9600)
+
+while True:
+    getMCUdata = str(openCommoLine.readline())
+    getTimeNow = str(datetime.datetime.now())
+    print(getMCUdata, getTimeNow)
+    file = open(CSVfileName, "a")
+    file.write(getMCUdata + getTimeNow + "\n")
+
+file.close()
