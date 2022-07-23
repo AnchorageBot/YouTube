e4d1617d01b7b796104448f5a5a395028721e77e
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2022-07-22 19:47:18 -0800

    Create RPi_PicoMorse3.py

---
+++ RPi_PicoMorse3.py
@@ -0,0 +1,40 @@
+# This script will flash SOS with the onboard LED of the RPi Pico
+
+# Reference
+  # Programming the Pico, Simon Monk
+  # https://github.com/simonmonk/prog_pico_ed1
+
+# Software
+  # Thonny 3.3.13
+  # MicroPython v1.18
+  
+# Hardware
+  # Raspberry Pi Pico with RP2040 and micro USB cable
+
+from machine import Pin
+from utime import sleep
+
+led = Pin(25, Pin.OUT)
+
+SleepVariable1 = 0.2
+SleepVariable2 = 0.6
+
+while True:
+    
+    # Code for the letter S = three dots
+    for x in range(1,4):
+        led.on()
+        sleep(SleepVariable1)
+        led.off()
+        sleep(SleepVariable1)
+    
+    # Time gap between S and O
+    sleep(SleepVariable2)
+    
+    # Code for the letter O = three dashes
+    for x in range(1,4):
+        led.on()
+        sleep(SleepVariable2)
+        led.off()
+        sleep(SleepVariable2)
+    
