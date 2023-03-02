a3a8fe14be5363b061e488be5e29a9e651be1dd8
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-03-02 12:33:57 -0900

    Create MicroBit_HelloWorld.py

---
+++ Microbit/MicroBit_HelloWorld.py
@@ -0,0 +1,18 @@
+# This script will display Hello World on the Microbit (V1, Firmware 0249, Sept 2021)
+
+# IDE (Integrated Development Environment)
+  # https://codewith.mu
+  
+# Bill of Materials
+  # Microbit v2 Go Kit
+  # https://www.adafruit.com/product/4834
+
+# Source code
+  # https://microbit-micropython.readthedocs.io/en/v1.0.1/tutorials/hello.html
+  
+# Engineering background  
+  # https://core-electronics.com.au/tutorials/micropython-workshop-for-microbit.html
+
+from microbit import *
+
+display.scroll("Hello, World!")
